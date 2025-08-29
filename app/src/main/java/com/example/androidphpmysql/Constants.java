bf323bfdc15f480ee66fb3700bb335668150cb8d
NabilZulfalah <nabilzulfalah@gmail.com>
2025-08-29 08:51:08 +0700

    starting point di sini (main)

--- app/src/main/java/com/example/androidphpmysql/Constants.java
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -1,7 +1,7 @@
 package com.example.androidphpmysql;
 
 public class Constants {
-    private static final String ROOT_URL = "http://172.16.100.247/Android/v1/";
+    private static final String ROOT_URL = "http://172.16.101.52/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
     public static final String URL_LOGIN = ROOT_URL + "loginUser.php";
