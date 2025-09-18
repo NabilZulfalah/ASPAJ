9ae95809784bbe014e7ab48ca6a6561d8151b6db
SakaEnd <sakaajasih@gmail.com>
2025-09-18 15:00:01 +0700

    main menu + list barang

--- app/src/main/java/com/example/androidphpmysql/activity_main_menu.kt
+++ app/src/main/java/com/example/androidphpmysql/activity_main_menu.kt
@@ -1,5 +1,6 @@
 package com.example.androidphpmysql
 
+import com.google.android.material.card.MaterialCardView
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -87,16 +88,16 @@ class activity_main_menu : AppCompatActivity() {
 
 
         // --- Tombol LinearLayout (opsional) ---
-        findViewById<LinearLayout>(R.id.listbarangBtn).setOnClickListener {
+        findViewById<MaterialCardView>(R.id.listbarangBtn).setOnClickListener {
             startActivity(Intent(this, activity_list_barang::class.java))
         }
-        findViewById<LinearLayout>(R.id.riwayatBtn).setOnClickListener {
+        findViewById<MaterialCardView>(R.id.riwayatBtn).setOnClickListener {
             startActivity(Intent(this, RiwayatActivity::class.java))
         }
-        findViewById<LinearLayout>(R.id.peminjamBtn).setOnClickListener {
+        findViewById<MaterialCardView>(R.id.peminjamBtn).setOnClickListener {
             startActivity(Intent(this, activity_data_peminjam::class.java))
         }
-        findViewById<LinearLayout>(R.id.pengembalianBtn).setOnClickListener {
+        findViewById<MaterialCardView>(R.id.pengembalianBtn).setOnClickListener {
             startActivity(Intent(this, pengembalianActivity::class.java))
         }
 
