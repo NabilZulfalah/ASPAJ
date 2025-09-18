43dc779909f51ebc0d5af5f552a815c610471373
Rizalfathi <rijalfahi303@gmail.com>
2025-09-18 13:22:32 +0700

    mengambil dari merge commit backend

--- app/src/main/java/com/example/androidphpmysql/Constants.java
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -1,7 +1,7 @@
 package com.example.androidphpmysql;
 
 public class Constants {
-    private static final String ROOT_URL = "http://192.168.4.83/Android/v1/";
+    private static final String ROOT_URL = "http://192.168.4.143/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
     public static final String URL_LOGIN = ROOT_URL + "loginUser.php";
