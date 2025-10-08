a7c5391214409df5179d0b4120d723cd6872df97
Jaka anwar <jaluanwar08@gmail.com>
2025-10-08 22:50:31 +0700

    tetstt

--- app/src/main/java/com/example/androidphpmysql/Constants.java
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -4,7 +4,7 @@ package com.example.androidphpmysql;
     private static final String ROOT_URL = "http://192.168.0.113/";
 
         public static final String URL_REGISTER = ROOT_URL + "API_PHP/v1/registerUser.php";
-        public static final String URL_LOGIN = ROOT_URL + "API_PHP/v1/loginUser.php";
+        public static final String URL_LOGIN = ROOT_URL + "API_PHP/v1/login.php";
 
         // Asset CRUD URLs
         public static final String URL_GET_ASSETS = ROOT_URL + "API_PHP/v1/getCommodities.php";
